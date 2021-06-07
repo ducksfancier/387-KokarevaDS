c772b03b4a0381c142b8bb8ed62fa97b4c3a55fb
ducksfancier <66886104+ducksfancier@users.noreply.github.com>
2021-06-08 01:52:12 +0300

    Create PR1_Kokareva.php

---
+++ 1_task/PR1_Kokareva.php
@@ -0,0 +1,48 @@
+<?php
+echo 'Задача 1';
+echo '<br>';
+$a = 7;
+$b = 3;
+$result = $a + $b;
+echo $result;
+?>
+
+<?php
+echo 'Задача 2';
+echo '<br>';
+$a = 7;
+$b = 3;
+$result = $a - $b;
+echo $result;
+?>
+
+<?php
+echo 'Задача 3';
+echo '<br>';
+$a = 7;
+$b = 3;
+$c = 11;
+$result = $a * $b * $c;
+echo $result;
+?>
+
+<?php
+echo 'Задача 4';
+echo '<br>';
+$a = 4;
+echo sqrt($a);
+?>
+
+<?php
+echo 'Задача 6';
+echo '<br>';
+$array = [1,2,3,4,5,6,7,8,9,10];
+var_dump($array); //Выводит информацию о переменной
+?>
+
+<?php
+echo 'Задача 7';
+echo '<br>';
+$array = [ 1 => 'сб',2 => 'вс'];
+var_dump($array);
+?>
